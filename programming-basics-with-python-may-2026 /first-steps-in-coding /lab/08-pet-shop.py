ac1c723c8b0df18b09be5b917b252f6265b3b7a5
glora-of-astora <glomatrova@gmail.com>
2026-06-10 22:08:26 +0300

    Create 08-pet-shop.py

---
+++ programming-basics-with-python-may-2026 /first-steps-in-coding /lab/08-pet-shop.py
@@ -0,0 +1,11 @@
+dog_food = 2.50
+cat_food = 4.00
+
+dog_food_pcs = int(input())
+cat_food_pcs = int(input())
+
+dog_food_total = dog_food * dog_food_pcs
+cat_food_total = cat_food * cat_food_pcs
+total_order = dog_food_total + cat_food_total
+
+print(f'{total_order} lv.')
