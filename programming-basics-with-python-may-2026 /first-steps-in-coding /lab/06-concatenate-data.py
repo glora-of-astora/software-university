f0ac2520750ff83a527c12533e4396b4c74d4c6f
glora-of-astora <glomatrova@gmail.com>
2026-06-10 22:12:18 +0300

    Update 06-concatenate-data.py

--- programming-basics-with-python-may-2026 /first-steps-in-coding /lab/06-concatenate-data.py
+++ programming-basics-with-python-may-2026 /first-steps-in-coding /lab/06-concatenate-data.py
@@ -3,4 +3,4 @@ lastName = input()
 age = int(input())
 town = input()
 
-print(f'You are {firstName} {lastName}, a {age}-years old person from {town}.')
+print(f"You are {firstName} {lastName}, a {age}-years old person from {town}.")
