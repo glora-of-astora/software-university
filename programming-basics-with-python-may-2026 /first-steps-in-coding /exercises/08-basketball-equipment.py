9cc284a61395f3ded66e24f1a7d678613797e0e8
glora-of-astora <glomatrova@gmail.com>
2026-06-11 17:56:48 +0300

    Create 08-basketball-equipment.py

---
+++ programming-basics-with-python-may-2026 /first-steps-in-coding /exercises/08-basketball-equipment.py
@@ -0,0 +1,10 @@
+yearly_fee = int(input())
+
+shoes = 0.6 * yearly_fee
+outfit = 0.8 * shoes
+ball = 0.25 * outfit
+accessories = 0.2 * ball
+
+expenses = yearly_fee + shoes + outfit + ball + accessories
+
+print(expenses)
